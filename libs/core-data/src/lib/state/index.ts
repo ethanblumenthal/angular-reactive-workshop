3c0b7d27dd68104623075a86913b99ac4d9d3e62
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-02-06 15:35:04 -0600

    Created reducers for projects.

--- libs/core-data/src/lib/state/index.ts
+++ libs/core-data/src/lib/state/index.ts
@@ -1,23 +1,31 @@
-import { ActionReducerMap, createFeatureSelector, createSelector } from '@ngrx/store';
+import {
+  ActionReducerMap,
+  createFeatureSelector,
+  createSelector
+} from '@ngrx/store';
 
 import * as fromCustomers from './customers/customers.reducer';
+import * as fromProjects from './projects/projects.reducer';
 
+// Updated the shape of the entire application state
 export interface AppState {
-  customers: fromCustomers.CustomersState
+  customers: fromCustomers.CustomersState;
+  projects: fromProjects.ProjectsState;
 }
-
+// Add in feature reducer into combined reducer
 export const reducers: ActionReducerMap<AppState> = {
-  customers: fromCustomers.customersReducer
+  customers: fromCustomers.customersReducer,
+  projects: fromProjects.projectsReducers
 };
 
 // -------------------------------------------------------------------
 // CUSTOMERS SELECTORS
 // -------------------------------------------------------------------
-export const selectCustomersState = createFeatureSelector<fromCustomers.CustomersState>('customers');
+export const selectCustomersState = createFeatureSelector<
+  fromCustomers.CustomersState
+>('customers');
 
 export const selectAllCustomers = createSelector(
   selectCustomersState,
   fromCustomers.selectAllCustomers
 );
-
-
--- libs/core-data/src/lib/state/projects/projects.reducer.ts
+++ libs/core-data/src/lib/state/projects/projects.reducer.ts
@@ -28,7 +28,48 @@ const initialProjects: Project[] = [
 ];
 
 const createProject = (projects, project) => [...projects, project];
-const updateProject = (projects, project) => projects.map(p => {
-  return p.id === project.id ? Object.assign({}, project) : p;
-});
-const deleteProject = (projects, project) => projects.filter(w => project.id !== w.id);
+
+const updateProject = (projects, project) =>
+  projects.map(p => {
+    return p.id === project.id ? Object.assign({}, project) : p;
+  });
+
+const deleteProject = (projects, project) =>
+  projects.filter(w => project.id !== w.id);
+
+export interface ProjectsState {
+  projects: Project[];
+  selectedProjectId: string | null;
+}
+
+export const initialState: ProjectsState = {
+  projects: initialProjects,
+  selectedProjectId: null
+};
+
+export function projectsReducers(state = initialState, action): ProjectsState {
+  switch (action.type) {
+    case 'select':
+      return {
+        selectedProjectId: action.payload,
+        projects: state.projects
+      };
+    case 'create':
+      return {
+        selectedProjectId: state.selectedProjectId,
+        projects: createProject(state.projects, action.payload)
+      };
+    case 'update':
+      return {
+        selectedProjectId: state.selectedProjectId,
+        projects: updateProject(state.projects, action.payload)
+      };
+    case 'delete':
+      return {
+        selectedProjectId: state.selectedProjectId,
+        projects: deleteProject(state.projects, action.payload)
+      };
+    default:
+      return state;
+  }
+}
